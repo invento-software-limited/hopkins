550aa12e412283b81dd1d84fe7cbaaa3b239a958
Md Omar Faruk <sakib@invento.com.bd>
2025-04-13 06:37:27 +0000

    Register Page Complete & Sitemap added

---
+++ hopkins/api/register.py
@@ -0,0 +1,27 @@
+import frappe
+from hopkins.api.cart import create_user
+from builder_ecommerce.cart import add_new_address, create_party, create_contact, update_address_with_customer
+
+
+@frappe.whitelist(allow_guest=True)
+def register(doc):
+    doc = frappe.parse_json(doc)
+    user = create_user(data=doc)
+    first_name = doc.get("first_name", "")
+    last_name = doc.get("last_name", "")
+    address_title = f"{first_name} {last_name}".strip()
+    doc["address_title"] = address_title
+    address = add_new_address(frappe.as_json(doc))
+
+    customer = {
+        "customer_name": doc.first_name + " " + doc.last_name,
+        "mobile_number": doc.telephone,
+        "customer_email_address": doc.email
+    }
+
+    party = create_party(doc=customer)
+    if party:
+        create_contact(doc, party.name)
+        if address:
+            update_address_with_customer(address.name, party.name)
+    return user
