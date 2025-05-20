b5c1638028605f14bad4cf58e0ef06606f85d0b5
Md Omar Faruk <sakib@invento.com.bd>
2025-05-20 10:49:19 +0000

    Removed Builder Customize

--- frontend/src/components/Controls/InputLabel.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<span class="inline-flex w-1/2 min-w-20 max-w-40 items-center text-xs leading-5 text-ink-gray-6">
-		<slot />
-	</span>
-</template>
--- frontend/src/data/builderSettings.ts
+++
@@ -1,9 +0,0 @@
-import { createDocumentResource } from "frappe-ui";
-
-const builderSettings = createDocumentResource({
-	doctype: "Builder Settings",
-	name: "Builder Settings",
-	cache: "builderSettings",
-});
-
-export { builderSettings };
